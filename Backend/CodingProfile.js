9556698b96d47d470a4768d14d3e454bc5904659
21A91A05J1 <21A91A05J1@aec.edu.in>
2024-03-19 21:19:57 +0530

    Final Draft

---
+++ Backend/CodingProfile.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const codingProfileSchema = new Schema({
+    user: {
+        type: String,
+        ref: 'User', // Reference to the User collection
+        unique: true
+    },
+    platforms: {
+        leetcode: { type: String, default: '' },
+        codechef: { type: String, default: '' },
+        geekforgeeks: { type: String, default: '' },
+        // Add more platforms as needed
+    }
+});
+
+const CodingProfile = mongoose.model('CodingProfile', codingProfileSchema);
+
+module.exports = CodingProfile;
